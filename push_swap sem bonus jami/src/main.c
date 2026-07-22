bdacd68324436e474e58c2ea46b36a6d830c338e
Jamielly Reis <100716307+Jamielly@users.noreply.github.com>
2026-07-22 15:57:22 -0300

    main

--- push_swap sem bonus jami/src/main.c
+++ push_swap sem bonus jami/src/main.c
@@ -12,27 +12,40 @@
 
 #include "push_swap.h"
 
-int	main(int argc, char **argv)
+static char	*get_strategy(int argc, char **argv)
 {
-	t_number	*stack_a;
-	t_number	*stack_b;
-	char		*strat;
+	int		i;
+	char	*strategy;
 
-	if (argc == 1)
-		return (0);
-	stack_b = NULL;
-	parse_input(argv, &stack_a, &strat);
-	if (!stack_a)
-		return (0);
-	while (argv[++i])
+	i = 1;
+	strategy = NULL;
+	while (i < argc)
 	{
 		if (ft_strncmp(argv[i], "--bench", 8) == 0)
 			get_bench()->bench_mode = 1;
 		else if (is_flag(argv[i]))
-			strat = argv[i];
+			strategy = argv[i];
+		i++;
 	}
+	return (strategy);
+}
+
+int	main(int argc, char **argv)
+{
+	t_number	*stack_a;
+	t_number	*stack_b;
+	char		*strategy;
+
+	if (argc <= 1)
+		return (0);
+	stack_a = NULL;
+	stack_b = NULL;
+	strategy = get_strategy(argc, argv);
+	parse_input(&stack_a, argv);
+	if (!stack_a)
+		return (0);
 	if (!is_sorted(stack_a))
-		execute_strategy(&stack_a, &stack_b, strat);
+		execute_strategy(&stack_a, &stack_b, strategy);
 	print_benchmark();
 	free_stack(&stack_a);
 	free_stack(&stack_b);
